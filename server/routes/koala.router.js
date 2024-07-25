bdffd75a66c0ba7f2db91d67092b596af0a08717
Elwood Nelson <elwoodnelson@gmail.com>
2024-07-25 13:29:26 -0500

    set up get route on server.js

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const koalaRouter = express.Router();
 
+const pool = require('../modules/pool.js')
+let koalaList = []
+
 // DB CONNECTION
 
 
 // GET
-
+router.get('/', (req, res) => {
+    let queryText = `
+    SELECT * FROM "koalas"  
+    `
+    pool.query(queryText)
+    .then(dbResult => {
+        res.send(koalaList.rows)
+    })
+    .catch(dbError => {
+        console.log('sql query error',dbError);
+        res.sendStatus(500);
+    })
+})
 
 // POST
 
